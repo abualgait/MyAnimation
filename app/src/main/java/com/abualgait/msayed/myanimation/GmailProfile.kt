372101ca6ebbb743a3b176d133ffc7c7c26220fb
m.sayed <abualgaitad@gmail.com>
2020-01-14 14:54:04 +0200

    - dayNight animation

--- app/src/main/java/com/abualgait/msayed/myanimation/GmailProfile.kt
+++ app/src/main/java/com/abualgait/msayed/myanimation/GmailProfile.kt
@@ -1,12 +1,32 @@
 package com.abualgait.msayed.myanimation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
 
 class GmailProfile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gmail_profile)
+
+        coordinateMotion()
     }
+
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
+    }
+
+
+
 }
--- app/src/main/java/com/abualgait/msayed/myanimation/MainActivity.kt
+++ app/src/main/java/com/abualgait/msayed/myanimation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.abualgait.msayed.myanimation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,10 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         button.setOnClickListener {
-            val fragment = FragmentBottomSheetDialogFull()
-            fragment.show(supportFragmentManager, fragment.tag)
+            startActivity(Intent(this, GmailProfile::class.java))
+
+        }
+
+        button2.setOnClickListener {
+            startActivity(Intent(this, DayNight::class.java))
+
+        }
+
+        button3.setOnClickListener {
+            startActivity(Intent(this, ProductiveFamilies::class.java))
+
         }
     }
 }
